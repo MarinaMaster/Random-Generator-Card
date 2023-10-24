1a1794d1176884b7572325429ee81152998a9123
Marina Master <marina777master@mail.ru>
2023-10-24 00:06:01 -0400

    complite Random Card Generator

--- src/app.js
+++ src/app.js
@@ -4,8 +4,36 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+import button from "bootstrap/js/src/button";
+let suites = ["♦", "♥", "♠", "♣"];
+let face = ["K", "J", "Q", "10", "9", "8", "7", "6", "5", "4", "3", "2", "A"];
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardGenerator = () => {
+    //write your code here
+    let topSuite = document.querySelector(".topSuite");
+    let number = document.querySelector(".number");
+    let buttomSuite = document.querySelector(".buttomSuite");
+    let randomFaceIndex = Math.floor(Math.random() * face.length);
+    let randomSuitesIndex = Math.floor(Math.random() * suites.length);
+    topSuite.innerHTML = suites[randomSuitesIndex];
+    number.innerHTML = face[randomFaceIndex];
+    buttomSuite.innerHTML = suites[randomSuitesIndex];
+
+    let randomSuites = suites[randomSuitesIndex];
+    if (randomSuites === "♥" || randomSuites === "♦") {
+      document.querySelectorAll(".suitColor").forEach(suitElement => {
+        suitElement.style.color = "red";
+      });
+    } else {
+      document.querySelectorAll(".suitColor").forEach(suitElement => {
+        suitElement.style.color = "black";
+      });
+    }
+  };
+  cardGenerator();
+
+  let buttonElement = document.querySelector(".btn");
+  buttonElement.addEventListener("click", () => {
+    cardGenerator();
+  });
 };
